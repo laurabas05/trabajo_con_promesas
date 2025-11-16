ddc9e2691f963a268295439f2775a95100f92483
laurabas05 <laaurabas@gmail.com>
2025-11-16 17:14:47 +0100

    Creada la función para guardar el objeto en el fichero

--- gastos.js
+++ gastos.js
@@ -3,7 +3,12 @@ const fs = require('fs').promises;
 const FICHERO = './datos.json';
 
 // funcion para leer el fichero y convertirlo a objeto
-function leerFichero() {
+function leerDatos() {
     return fs.readFile(FICHERO, 'utf8')
         .then(texto => JSON.parse(texto));
+}
+
+// funcion para guardar el objeto en el fichero
+function guardarDatos(datos) {
+    return fs.writeFile(FICHERO, JSON.stringify(datos, null, 2));
 }
